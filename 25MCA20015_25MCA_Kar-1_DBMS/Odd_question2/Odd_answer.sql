2ccff11b047cc76a657191c1ec00ba300fff8a24
Swayam <aroraswayam992@gmail.com>
2026-03-10 12:04:40 +0530

    Create Employee table and HighSalaryEmployees view

---
+++ 25MCA20015_25MCA_Kar-1_DBMS/Odd_question2/Odd_answer.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Employee (
+    EmpID INT PRIMARY KEY,
+    EmpName VARCHAR(50),
+    Salary INT
+);
+INSERT INTO Employee (EmpID, EmpName, Salary) VALUES
+(1, 'Swayam', 60000),
+(2, 'Roshan', 45000),
+(3, 'Sanchit', 52000),
+(4, 'Aman', 40000),
+(5, 'Rahul', 75000),
+(6, 'Priya', 68000),
+(7, 'Karan', 30000),
+(8, 'Neha', 54000),
+(9, 'Rohit', 49000),
+(10, 'Ananya', 82000);
+
+CREATE VIEW HighSalaryEmployees AS
+SELECT EmpID, EmpName, Salary
+FROM Employee
+WHERE Salary > 50000;
+
+SELECT * FROM HighSalaryEmployees;
