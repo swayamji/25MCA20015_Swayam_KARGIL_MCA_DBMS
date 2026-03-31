e1c9ae8cb39ed5b164c6549f9616bc825c6d8010
Swayam <aroraswayam992@gmail.com>
2026-03-31 12:47:56 +0530

    Create leetcode.sql

---
+++ Experiment 8/leetcode.sql
@@ -0,0 +1,9 @@
+--Confirmation Rate
+
+SELECT
+  Signups.user_id,
+  IFNULL(ROUND(AVG(Confirmations.action = 'confirmed'), 2), 0) AS confirmation_rate
+FROM Signups
+LEFT JOIN Confirmations
+  USING (user_id)
+GROUP BY 1;
